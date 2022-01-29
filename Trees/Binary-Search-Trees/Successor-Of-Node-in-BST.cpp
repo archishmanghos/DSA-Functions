155f5e6f5f2a8e4688dd8eba75a8d46fdc702704
Archishman Ghosh <ag2324@it.jgec.ac.in>
2022-01-29 12:22:22 +0530

    Add function to find the successor and predecessor of a node in a BST

---
+++ Trees/Binary-Search-Trees/Successor-Of-Node-in-BST.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+#define int long long
+using namespace std;
+
+const int mxN = 2e5 + 5;
+const int INF = 1e18 + 7;
+
+int value;
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+
+    Node(int input) {
+        data = input;
+        left = right = NULL;
+    }
+};
+
+Node *buildTree() {
+    cin >> value;
+    if (value == -1)
+        return NULL;
+
+    Node *root = new Node(value);
+    root->left = buildTree();
+    root->right = buildTree();
+
+    return root;
+}
+
+Node *inorderSuccessorinBST(Node *root, Node *givenNode) {
+    Node *successor = NULL;
+    while (root) {
+        if (root->data > givenNode->data) {
+            successor = root;
+        }
+        root = (root->data > givenNode->data ? root->left : root->right);
+    }
+
+    return successor;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    Node *root = buildTree();
+    Node *givenNode = new Node(5);
+    Node *ans = inorderSuccessorinBST(root, givenNode);
+    cout << (ans ? ans->data : -1);
+
+    return 0;
+}
