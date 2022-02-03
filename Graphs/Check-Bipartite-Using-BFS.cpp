7d3d138c1ee15a24538f098be26faaa13c73e4ec
Archishman Ghosh <ag2324@it.jgec.ac.in>
2022-02-03 12:04:36 +0530

    Add function to check if graph is BiPartite using BFS

---
+++ Graphs/Check-Bipartite-Using-BFS.cpp
@@ -0,0 +1,56 @@
+int N, M, U, V;
+vector<int> adj[mxN], colour(mxN);
+
+bool checkCanBeColoured(int node) {
+    queue<int> q;
+    q.push(node);
+    colour[node] = 0;
+
+    bool ans = true;
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        for (int child: adj[node]) {
+            if (colour[child] == -1) {
+                colour[child] = 1 - colour[node];
+                q.push(child);
+            } else {
+                if (colour[child] == colour[node]) {
+                    ans = false;
+                }
+            }
+        }
+    }
+
+    return ans;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+
+    cin >> N >> M;
+    for (int i = 1; i <= M; i++) {
+        cin >> U >> V;
+        adj[U].push_back(V);
+        adj[V].push_back(U);
+    }
+
+    colour.assign(mxN, -1);
+    for (int i = 1; i <= N; i++) {
+        if (colour[i] == -1) {
+            if (checkCanBeColoured(i)) {
+                cout << "YES" << '\n';
+            } else {
+                cout << "NO" << '\n';
+            }
+        }
+    }
+    return 0;
+}
