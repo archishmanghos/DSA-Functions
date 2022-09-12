832cd1bb0393936eddf4b06eda19ba54d9e07ed8
Archishman Ghosh <ag2324@it.jgec.ac.in>
2022-09-12 19:04:56 +0530

    changed segment tree file

--- Advance-DS/Segment Tree full.cpp
+++ Advance-DS/Segment Tree full.cpp
@@ -1,55 +1,68 @@
-//Building Segment Tree
+class SegTree {
+private:
+	int n;
+	vector<int64_t> A;
 
-void build_Segment_Tree(int node,int start,int end)
-{
-	if(start==end)
-	{
-		tree[node]=A[start];
+	void build(int index, int l, int r, vector<int> &v) {
+		if (l == r) {
+			if (l <= v.size()) {
+				A[index] = v[l];
+			}
+			return;
+		}
+
+		int mid = l + (r - l) / 2;
+		build(2 * index, l, mid, v);
+		build(2 * index + 1, mid + 1, r, v);
+		A[index] = A[2 * index] + A[2 * index + 1]; // change with the problem's function
 	}
-	else
-	{
-		int mid=start + (end - start) / 2;
-		build_Segment_Tree((2*node),start,mid);
-		build_Segment_Tree((2*node+1),mid+1,end);
-		tree[node]=tree[2*node]+tree[2*node+1];
+
+	int query(int index, int l, int r, int lx, int rx) {
+		if (l >= lx and r <= rx) return A[index];
+		if (r < lx or l > rx) return 0;
+		int mid = l + (r - l) / 2;
+		return query(2 * index, l, mid, lx, rx) + query(2 * index + 1, mid + 1, r, lx, rx);
 	}
-}
 
-// Update on Segment tree
+	void update(int index, int l, int r, int idx, int val) {
+		if (l == r) {
+			A[index] = val;
+			return;
+		}
 
-void update_Segment_Tree(int idx,int val,int node,int start, int end)
-{
-	if(start==end)
-	{
-		A[idx]+=val;
-		tree[node]+=val;
+		int mid = l + (r - l) / 2;
+		if (idx <= mid) update(2 * index, l, mid, idx, val);
+		else update(2 * index + 1, mid + 1, r, idx, val);
+		A[index] = A[2 * index] + A[2 * index + 1]; // change with the problem's function
 	}
-	else
-	{
-		int mid=start + (end - start) / 2;
-		if(start<=idx&&idx<=mid)
-			update_Segment_Tree(idx,val,(2*node),start,mid);
-		else
-			update_Segment_Tree(idx,val,(2*node+1),mid+1,end);
-		tree[node]=tree[2*node]+tree[2*node+1];
+
+public:
+	void init(int n1) {
+		n = 1;
+		while (n < n1) n <<= 1;
+		A.assign(2 * n, 0);
 	}
-}
 
-// Query on Segment tree
-
-int query_Segment_Tree(int l, int r,int node,int start,int end)
-{
-	if(r<start||end<l)
-		return 0;
-	else if(l<=start&&end<=r)
-		return tree[node];
-	else
-	{
-		int mid=start + (end - start) / 2;
-		int x1=query_Segment_Tree(l,r,(2*node),start,mid);
-		int x2=query_Segment_Tree(l,r,(2*node+1),mid+1,end);
-		return (x1+x2);
+	void build(vector<int> &A) {
+		build(1, 1, n, A);
 	}
-}
 
-//End of Segment tree
+	int64_t query(int l, int r) {
+		return query(1, 1, n, l, r);
+	}
+
+	void update(int idx, int val) {
+		update(1, 1, n, idx, val);
+	}
+
+	void print() {
+		for (int i = 1; i < 2 * n; i++) {
+			cout << i << ": " << A[i] << '\n';
+		}
+	}
+};
+
+// All arrays are 1 based indexed and should alse be same in main function.
+// Init should be called as soon as this class is called.
+// Remember to build the tree before operations.
+// Use print function in case of any discrepancy.
